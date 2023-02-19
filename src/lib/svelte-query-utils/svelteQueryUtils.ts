c64f379c970c074632832dce3b4ba7fe02f9370a
DaniAkash <s.daniakash@gmail.com>
2023-02-19 15:09:31 +0530

    chore: update create client

--- src/lib/svelte-query-utils/svelteQueryUtils.ts
+++ src/lib/svelte-query-utils/svelteQueryUtils.ts
@@ -3,4 +3,12 @@ import { createClient } from '../generated/client';
 import type { Operations } from '../generated/client';
 const client = createClient(); // Typesafe WunderGraph client
 
-export const { createQuery } = createQueryUtils<Operations>(client);
+export const {
+	createQuery,
+	createFileUpload,
+	createMutation,
+	createSubscription,
+	getAuth,
+	getUser,
+	queryKey
+} = createQueryUtils<Operations>(client);
