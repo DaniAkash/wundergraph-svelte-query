1cfe753ab1e36f2bd37211b07ad1e59c4e6c5dd1
Dani Akash <DaniAkash@users.noreply.github.com>
2023-04-06 12:26:52 +0530

    chore: added todo for SSR support

--- sveltekit/src/routes/+page.ts
+++ sveltekit/src/routes/+page.ts
@@ -1,10 +1,8 @@
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
-export const prerender = true;
+export const prerender = false;
 
 import type { PageLoad } from './$types'
 
 export const load: PageLoad = async ({ parent }) => {
     const { queryClient } = await parent()
-
+    // TODO: Add prefetching logic here once SSR support is published in npm
 }
